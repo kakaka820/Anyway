a8520dff39690b48a2fce7ba59061e12a24c93ab
kakaka820 <tata_ta@163.com>
2026-04-05 16:10:40 +0800

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ import Phaser from 'phaser';
 import { BootScene } from './scenes/BootScene';
 import { TitleScene } from './scenes/TitleScene';
 import { RoomScene } from './scenes/RoomScene';
+import { UIScene } from './scenes/UIScene';
 
 new Phaser.Game({
   type: Phaser.AUTO,
   width: 800,
   height: 600,
   backgroundColor: '#1a1a2e',
-  scene: [BootScene, TitleScene, RoomScene],
+  scene: [BootScene, TitleScene, RoomScene, UIScene],
 });
